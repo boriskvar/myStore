5770849129885e11ccaf3665959ef417c808ecd8
boris <125734087+boriskvar@users.noreply.github.com>
2023-04-24 16:42:51 +0300

    first

---
+++ wp-content/themes/myStore/footer.php
@@ -0,0 +1,21 @@
+	<!-- Футер -->
+	<div class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 text-center">
+					<?php 
+						wp_nav_menu([
+							'theme_location' => 'bottom',
+							'container' => '',
+							'items_wrap' => '%3$s'
+						]);
+					?>
+					<span class="accent-color">&copy; </span> <?= bloginfo('name'); ?>, 1996-<?= date('Y'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php wp_footer(); ?>
+</body>
+
+</html>
